b02760db389342f4baaeb8ebc8bc14fb3a1b6907
ScottEParker <35380990+ScottEParker@users.noreply.github.com>
2018-02-12 23:09:13 -0800

    Implement minimum

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -28,11 +28,12 @@ describe("About Applying What We Have Learnt", function() {
                   hasMushrooms = true;
                }
             }
-            if (!hasMushrooms) productsICanEat.push(products[i]);
+            if (!hasMushrooms)
+                productsICanEat.push(products[i]);
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +42,7 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(0);
   });
 
   /*********************************************************************************/
